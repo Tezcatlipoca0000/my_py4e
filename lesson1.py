d2848aef6a35ab5581c84eab91861b2ce580cd61
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 16:17:48 -0500

    py4e lesson 1

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# Lesson 1
+# My first python file
+
+# Assignment 
+x = 7
+x = x + 1
+
+# Conditional 
+if x < 10:
+    print('first condition true')
+if x > 10:
+    print('second condition true')
+
+# Repeated 
+while x < 10:
+    print('in a loop')
+    x = x + 1
+
+print('finished my first file')
