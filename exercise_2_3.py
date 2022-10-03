d5bf542358ed9b8aae45a0b8da790478fb6c056d
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 16:35:51 -0500

    exercise 2.3

---
+++ exercise_2_3.py
@@ -0,0 +1,5 @@
+# https://www.py4e.com/html3/02-variables
+# Exercise 2.3 Write a program to prompt the user for hours and rate per hour to compute gross pay.
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+print("Pay:", float(hours) * float(rate))
