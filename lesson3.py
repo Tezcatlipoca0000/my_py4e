c161dcda32501d132c480bf55985b67f9cb61dcf
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 16:36:43 -0500

    Lesson 3

---
+++ lesson3.py
@@ -0,0 +1,22 @@
+# Lesson 3 - Conditional execution
+
+# Comparison operators
+x = 5
+if x == 5:
+    print('equal than')
+if x >= 5:
+    print('greater than or equal')
+if x > 4:
+    print('greater than')
+if x <= 5:
+    print('less than or equal')
+if x < 6:
+    print('less than')
+if x != 6:
+    print('not equal than')
+
+# else
+if x == 5:
+    print('if true')
+else:
+    print('else')
