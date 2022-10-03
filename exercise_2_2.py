1ed82cc5c153f6bda96b3e50c48b934c2abec5d9
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 16:35:06 -0500

    exercise 2.2

---
+++ exercise_2_2.py
@@ -0,0 +1,4 @@
+# https://www.py4e.com/html3/02-variables
+# Exercise 2.2 Write a program that uses input to prompt a user for their name and then welcomes them.
+name = input("What's your name? ")
+print('Hello', name)
