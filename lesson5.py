db137fc0fbd20cdc1edb7115012e92531346cba7
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 22:30:36 -0500

    Lesson 5

---
+++ lesson5.py
@@ -0,0 +1,16 @@
+# Lesson 5 Functions
+
+# def
+def myFirstFun():
+    print('My first defined function!')
+    print('Hurra!')
+
+myFirstFun()
+
+# max built-in-function
+big = max('Hello world')
+print('max output', big)
+
+# min built-in-function
+tiny = min('Hello world')
+print('min output', tiny)
