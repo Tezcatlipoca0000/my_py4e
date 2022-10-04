63f6b84987b5161a78159a0f072cd51800e15fee
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-03 22:32:49 -0500

    Lesson 7

---
+++ lesson7.py
@@ -0,0 +1,19 @@
+# Lesson 7 Loops and iterations
+
+# break
+while True :
+    line = input('>>> ')
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
+
+# continue
+while True :
+    line = input('>>> ')
+    if line[0] == '#' :
+        continue
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
