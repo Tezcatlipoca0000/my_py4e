9f170db98f4949701dd87e120d2100fdc92fad53
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-07 17:49:31 -0500

    Lesson 15

--- lesson15.py
+++ lesson15.py
@@ -1,6 +1,15 @@
-# Lesson 15 python lists
+# Lesson 15 python lists (collection | list == Array)
 
-# collection | list == Array
 friends = ['Arnold', 'Bob', 'Chuck']
+print('the collection', friends)
 for friend in friends :
     print(friend)
+
+print('the range function', range(5))
+for i in range(len(friends)) :
+    print('iteration', i)
+    print('friend', friends[i])
+
+# list are mutable unlike string
+friends[1] = ['Benjamin', 'Barney']
+print(friends)
