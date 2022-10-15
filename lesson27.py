ab7ebbe55935975074b554ffaa06d41e30fdb1f4
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-15 17:16:30 -0500

    Lesson 27

--- lesson27.py
+++ lesson27.py
@@ -18,3 +18,16 @@ mysocket.close()
 
 # NOTICE the end='' parameter in the print statement
 # Without it text looks funny 
+
+# Experiment 1 >>>>>>> returns bad request
+mysocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket2.connect(('freecodecamp.org',80))
+cmd2 = 'GET https://www.freecodecamp.org/learn/ HTTP/1.0\r\n\r\n'.encode()
+mysocket2.send(cmd2)
+
+while True :
+    data2 = mysocket2.recv(512)
+    if len(data2) < 1 : break
+    print(data2.decode(),end='')
+
+mysocket2.close() 
