9ecb0f940f4f38be16a1f8c5996d6beacb982a18
Tezcatlipoca <tezcatlipoca0000@gmail.com>
2022-10-19 15:30:04 -0500

    Lesson 31

---
+++ lesson31.py
@@ -0,0 +1,33 @@
+# Lesson 31 JSON
+
+import json
+
+data = '''{
+    "key1": "string val",
+    "key2": {
+        "sub_key1": 8
+    },
+    "key3": ["array", 2]
+}'''
+info = json.loads(data)
+print(type(info)) # <class 'dict'>
+print(info)
+for k,v in info.items() :
+    print(k,v)
+
+
+data2 = '''[
+    {
+        "key1": "val1",
+        "key2": 2
+    },
+    {
+        "key1": "val2.1",
+        "key2": 2 
+    }
+]'''
+info2 = json.loads(data2)
+print(type(info2))
+print(info2)
+for obj in info2 :
+    print(obj)
