c7d70163a4200689f0567b4a7b0c0e6f3870706f
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-10-29 12:48:41 -0500

    Exercise 15.1

---
+++ exercise_15_1.py
@@ -0,0 +1,10 @@
+# Exercise 15.1 Database Email
+
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
